7e4de6a92f90657eb9a2c16cf74abc55076e6b8c
Krishna-333 <SaiKrishna.Pulivarthi@suresby.net>
2020-01-30 19:47:05 +0530

    added geturl for each function

--- Tests/homepagetest.js
+++ Tests/homepagetest.js
@@ -11,13 +11,15 @@ describe('Testing landing page buttons', function()
     );
 
     it('testing sign up button', function()
-        {
+        {   
+            landingpage.get("http://172.22.0.72:8085/#/");
             landingpage.clickonsignup();
             landingpage.verifysignupclick();
         });
 
     it('verify logo click', function()
-        {
+        {   
+            landingpage.get("http://172.22.0.72:8085/#/");
             landingpage.logoclick();
             landingpage.verifylogoclick();
         });
